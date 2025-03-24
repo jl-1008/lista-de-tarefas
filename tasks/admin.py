3ee4d3c474fc9474c71bb26a4b1436c27a71f329
JorgeLeandro <jorgeleandro1008@gmail.com>
2025-03-23 23:24:40 -0300

    Corrigido list_display e adicionado comentário no admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Task    
 
 # Registra o modelo Task no painel admin
-admin.site.register(Task)    
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'completed', 'created')  # Mostra título, status e data na lista
 
 
 
