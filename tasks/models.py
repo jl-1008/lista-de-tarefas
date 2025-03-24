770540f2f367ebb3ab6526f85fa3cd686a5c830b
JorgeLeandro <jorgeleandro1008@gmail.com>
2025-03-24 19:45:31 -0300

    Corrigido nome do campo completed no modelo Tasks

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ class Task(models.Model):
     # Campo pra título da tarefa, até 200 caracteres
     title = models.CharField(max_length=200)
     # Indica se a tarefa tá concluída, padrão é falso
-    complete = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     # Data de criação da tarefa, preenchida automaticamente
     created = models.DateTimeField(auto_now_add=True)
 
