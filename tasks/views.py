b4082cac3b82268f9ed3c47288a41013b4b0f4ce
JorgeLeandro <jorgeleandro1008@gmail.com>
2025-03-25 23:52:40 -0300

    Criada view task_list para listar tarefas no front-end

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Task
+# Importa o medelo Task para pegar os dados
 
-# Create your views here.
+
+# Função que lista todas as tarefas e manda para o template
+def task_list(request):
+    tasks = Task.object.all() #Busca todas as tarefas no banco de dados
+    return render(request, 'tasks/task_list.html' , {'tasks': tasks}) # Renderiza o template com as tarefas
