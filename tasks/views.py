6ac99e718a28fa2fd46643f5c3484127011ced04
JorgeLeandro <jorgeleandro1008@gmail.com>
2025-04-07 21:51:15 -0300

    Atualizados templates HTML, views.py e adicionados forms.py e task_complete.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404  # Adicionadas as importações necessárias
 from .models import Task
-
+from .forms import TaskForm
 
 def task_detail(request, pk): # Função que mostra os detalhes de uma tarefa
     task = get_object_or_404(Task, pk=pk)  # Pega a tarefa pelo id
@@ -15,11 +15,13 @@ def task_list(request):
 
 def task_new(request): #criando uma nova tarefa
     if request.method == 'POST':  # Se o método for POST
-        title = request.POST.get('title')  # Pega o título do formulário
-        if title:
-            Task.objects.create(title=title)  # Corrigido: "Task" com "T" maiúsculo
-            return redirect('task_list')  # Redireciona para a lista de tarefas
-    return render(request, 'tasks/task_new.html')  # Renderiza o template do formulário de nova tarefa
+        form = TaskForm(request.POST)  # Cria o formulário com os dados enviados
+        if form.is_valid():  # Verifica se os dados são válidos
+            form.save()  # Salva a tarefa no banco de dados
+            return redirect ('task_list')  # Renderiza o template do formulário de nova tarefa
+    else:  
+        form = TaskForm()  # Cria um novo formulário vazio GET
+    return render(request, 'tasks/task_new.html', {'form': form})# Passa o formulário para o template
 
 def task_edit(request, pk):
     task = get_object_or_404(Task, pk=pk)  # Pega a tarefa pelo id
@@ -42,6 +44,9 @@ def task_delete(request, pk):
 
 def task_complete(request, pk):
     task = get_object_or_404(Task, pk=pk)  # Pega a tarefa pelo id
-    task.completed = True
-    task.save()  # Marca a tarefa como concluída
-    return redirect('task_list')  # Redireciona para a lista de tarefas
+    if request.method == "POST": # Marca a tarefa como concluída
+       task.completed = True
+       task.save()  # Marca a tarefa como concluída
+       return redirect('task_list')  # Redireciona para a lista de tarefas  
+    return render(request, 'tasks/task_complete.html', {'task': task})  # Renderiza o template de confirmação de conclusão
+# Adicionei a função task_complete para marcar uma tarefa como concluída                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
